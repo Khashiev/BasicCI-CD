d3c6cee31b17fe2bb7bd41d3d63324b31daa4363
Khashiev <nxashiev@gmail.com>
2023-11-21 19:56:40 +0300

    part-5 succes 6

--- src/copy.sh
+++ src/copy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-scp -o StrictHostKeyChecking=no src/cat/s21_cat user2@192.168.0.111:/usr/local/bin
+echo "lala"
+
+scp src/cat/s21_cat user2@192.168.0.111:/usr/local/bin
 if [ $? -ne 0 ]; then echo "Copy error 1" && exit 1; fi
-scp -o StrictHostKeyChecking=no src/grep/s21_grep user2@192.168.0.111:/usr/local/bin
+
+scp src/grep/s21_grep user2@192.168.0.111:/usr/local/bin
 if [ $? -ne 0 ]; then echo "Copy error 2" && exit 1; fi
