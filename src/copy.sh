77912defd91034ccb01267ecae5235f232390cbb
Khashiev <nxashiev@gmail.com>
2023-11-22 10:43:38 +0300

    part-5 succes 8

--- src/copy.sh
+++ src/copy.sh
@@ -2,6 +2,5 @@
 
 scp src/cat/s21_cat user2@192.168.0.111:/usr/local/bin
 if [ $? -ne 0 ]; then echo "Copy error 1" && exit 1; fi
-
 scp src/grep/s21_grep user2@192.168.0.111:/usr/local/bin
 if [ $? -ne 0 ]; then echo "Copy error 2" && exit 1; fi
