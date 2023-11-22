c69b2c4b08411dd41da77f536652455292905f33
Khashiev <nxashiev@gmail.com>
2023-11-22 17:36:20 +0300

    part-6 final

--- src/tg.sh
+++ src/tg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 URL="https://api.telegram.org/bot6401202807:AAHaV99a615PoGglyi1tHopGiqEDxnbTzgA/sendMessage"
-TEXT="$CI_JOB_STAGE%0Astatus:+$CI_JOB_STATUS%0A"
+TEXT="stage:+$CI_JOB_STAGE%0Astatus:+$CI_JOB_STATUS%0A"
 
 curl -s -d "chat_id=468308894&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
